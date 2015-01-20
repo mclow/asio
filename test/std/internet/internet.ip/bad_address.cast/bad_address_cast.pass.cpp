080e36a3797ea8f3761602d37d1f3ac98836ab0e
Marshall Clow <mclow.lists@gmail.com>
2015-01-20 08:24:55 -0800

    add a test for bad_address_cast

---
+++ test/std/internet/internet.ip/bad_address.cast/bad_address_cast.pass.cpp
@@ -0,0 +1,33 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// namespace std {  namespace experimental { inline namespace network_v1 {  namespace ip {
+//
+//         class bad_address_cast : bad_cast
+//         {
+//         public:
+//           virtual const char* what() const noexcept;
+//         };
+//
+// }}}} // namespace std::experimental::network_v1::ip
+// 
+
+
+
+#include <experimental/internet>
+#include <cassert>
+
+namespace ip = std::experimental::network_v1::ip;
+
+int main()
+{
+    static_assert ( std::is_base_of<std::bad_cast, ip::bad_address_cast>::value, "" );
+    static_assert ( noexcept(((ip::bad_address_cast *)0)->what()), "" );
+}
